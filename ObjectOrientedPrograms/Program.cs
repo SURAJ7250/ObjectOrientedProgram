9288364c244ea9558b327d15d7e1e4f1fa058ef4
surajbridgelabz <surajnsinha@gmail.com>
2022-09-13 23:06:41 +0530

    [suraj] Add: create Inventory Management System

--- ObjectOrientedPrograms/Program.cs
+++ ObjectOrientedPrograms/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ObjectOrientedPrograms.InventoryManagement;
+using ObjectOrientedPrograms.InventoryManagementSystem;
 
 namespace ObjectOrientedPrograms
 {
@@ -6,17 +7,37 @@ namespace ObjectOrientedPrograms
     {
         //given inventory path to json
         const string INVENTORY_DATA_FILE_PATH = @"C:\Users\Suraj Sinha\OneDrive\Desktop\Bridgelabz\ObjectOrientedProgram\ObjectOrientedPrograms\InventoryManagement\Inventory.json";
+        const string INVENTORYDETAILS_DATA_FILE_PATH = @"C:\Users\Suraj Sinha\OneDrive\Desktop\Bridgelabz\ObjectOrientedProgram\ObjectOrientedPrograms\InventoryManagementSystem\InventoryDetails.json";
         static void Main(string[] args)
         {
             while (true)
             {
-                Console.WriteLine("Select Programs\n 1.InventoryManagement");
+                Console.WriteLine("Select Programs\n 1.InventoryManagement\n 2.InventoryManagementSystem");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
                     case 1:
                         Inventory inventory = new Inventory();
                         inventory.ReadJsonFile(INVENTORY_DATA_FILE_PATH);
+                        break;
+                    case 2:
+                        InventoryFactory inventoryFactory = new InventoryFactory();
+                        inventoryFactory.ReadJsonFile(INVENTORYDETAILS_DATA_FILE_PATH);
+                        InventoryDetails details = new InventoryDetails()
+                        {
+                            Name = "Basmati",
+                            weight = 10,
+                            price = 20,
+                        };
+                        //Add Inventory
+                        inventoryFactory.AddInventory("RiceList", details);
+                        inventoryFactory.WriteToJson(INVENTORYDETAILS_DATA_FILE_PATH);
+                        //Delete Inventory
+                        inventoryFactory.DeleteInventory("RiceList", "Basmati");
+                        inventoryFactory.WriteToJson(INVENTORYDETAILS_DATA_FILE_PATH); 
+                        //Edit Inventory
+                        inventoryFactory.EditInventory("RiceList", "Basmati");
+                        inventoryFactory.WriteToJson(INVENTORYDETAILS_DATA_FILE_PATH);
                         break;
                 }
             }
